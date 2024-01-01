ca507f9e241b482ac4429d626ed3b8dd22cb30bd
Gordon_Main_pc <pintadezombie@gmail.com>
2024-01-01 21:38:18 +0100

    Push ajout environnement de test

---
+++ TestResa/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestResa
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // ajout ligne test
+        }
+    }
+}
