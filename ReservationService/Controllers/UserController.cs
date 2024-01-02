72b8cdcca328d452288000c01bb026b536244719
GordonTousqueta <97089663+GordonTousqueta@users.noreply.github.com>
2024-01-02 15:36:15 +0100

    Message

---
+++ ReservationService/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReservationService.Datas.Repository;
+using System.Net;
+
+namespace ReservationService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        private readonly ILogger<UserController> _logger;
+        private readonly IReservationRepository _reservationRepository;
+        public UserController(ILogger<UserController> logger, IReservationRepository reservationRepository)
+        {
+            _logger = logger;
+            reservationRepository = _reservationRepository;
+        }
+
+        [HttpGet("{testAsync}")]
+        public async Task<ActionResult> testAsync()
+        {
+            var test = await _reservationRepository.GetReservationAsync();
+            return Ok(test);
+        }
+
+
+
+    }
+}
